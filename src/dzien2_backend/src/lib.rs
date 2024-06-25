883ff8599fd5fe67970f587ac1958d2d3414c193
koloso-s <zarychkrystian@gmail.com>
2024-06-25 18:55:02 +0200

    added way to add wpisy

--- src/dzien2_backend/src/lib.rs
+++ src/dzien2_backend/src/lib.rs
@@ -1,4 +1,19 @@
+use std::cell::RefCell;
+
+thread_local! {
+    static WPISY: RefCell<Vec<String>> = RefCell::default();
+}
+
 #[ic_cdk::query]
 fn greet(name: String, last_name: i8) -> String {
     format!("Hello, {} {}!", name, last_name)
+}
+
+#[ic_cdk::update]
+fn dodaj_wpis(wpis: String){
+    // let wpis2: String = wpis;
+    // let wpis3: String = wpis;
+    WPISY.with(|wpisy|{
+        wpisy.borrow_mut().push(wpis)
+    });
 }
